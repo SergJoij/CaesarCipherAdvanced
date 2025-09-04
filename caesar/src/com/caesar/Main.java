02460a86cb5bb9c0e72f164efeb88316412b28c2
SergJoij <135418103+SergJoij@users.noreply.github.com>
2025-09-04 17:53:35 +0400

    Убраны лишние комментарии в Main.java

--- caesar/src/com/caesar/Main.java
+++ caesar/src/com/caesar/Main.java
@@ -3,9 +3,8 @@ package com.caesar;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         Inter form = new Inter(); // объект form класса inter
         form.setVisible(true);
-
     }
 }
+
