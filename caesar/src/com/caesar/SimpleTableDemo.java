bdccac9df6f5cb4181e80bfd65423702ebb4f3c3
SergJoij <135418103+SergJoij@users.noreply.github.com>
2025-09-04 17:53:02 +0400

    Убраны лишние комментарии в SimpleTableDemo.java

--- caesar/src/com/caesar/SimpleTableDemo.java
+++ caesar/src/com/caesar/SimpleTableDemo.java
@@ -44,10 +44,7 @@ public class SimpleTableDemo extends JPanel {
             });
         }
 
-        //Create the scroll pane and add the table to it.
         JScrollPane scrollPane = new JScrollPane(table);
-
-        //Add the scroll pane to this panel.
         add(scrollPane);
     }
 
@@ -73,23 +70,18 @@ public class SimpleTableDemo extends JPanel {
      * event-dispatching thread.
      */
     private static void createAndShowGUI() {
-        //Create and set up the window.
         JFrame frame = new JFrame("SimpleTableDemo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Create and set up the content pane.
         SimpleTableDemo newContentPane = new SimpleTableDemo();
         newContentPane.setOpaque(true); //content panes must be opaque
         frame.setContentPane(newContentPane);
 
-        //Display the window.
         frame.pack();
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
@@ -97,3 +89,4 @@ public class SimpleTableDemo extends JPanel {
         });
     }
 }
+
